b3498b1d1622731b18a3305df48b3aec236edb71
sergey <dazranagon@yandex.ru>
2020-01-18 15:23:07 +0300

    refactoring adding dependencies

--- drf_dx_datagrid/viewsets.py
+++ drf_dx_datagrid/viewsets.py
@@ -14,36 +14,41 @@ class DxModelViewSet(rest_framework.viewsets.ModelViewSet, DxMixin):
         queryset = self.filter_queryset(self.get_queryset())
         group = self.get_param_from_request(request, "group")
         if group is None:
-            page = self.paginate_queryset(queryset)
-            if page is not None:
-                serializer = self.get_serializer(page, many=True)
-                return self.get_paginated_response(serializer.data)
-
-            serializer = self.get_serializer(queryset, many=True)
-            return Response({"data": serializer.data})
+            return self._not_grouped_list(queryset)
         else:
-            require_group_count = self.get_param_from_request(request, "requireGroupCount")
-            require_total_count = self.get_param_from_request(request, "requireTotalCount")
-            if group[0]["isExpanded"]:
-                return Response()
+            return self._grouped_list(group, queryset, request)
+
+    def _grouped_list(self, group, queryset, request):
+        require_group_count = self.get_param_from_request(request, "requireGroupCount")
+        require_total_count = self.get_param_from_request(request, "requireTotalCount")
+        if group[0]["isExpanded"]:
+            return Response()
+        else:
+            group_field_name = group[0]["selector"].replace(".", "__")
+            ordering = self.get_ordering(group)
+            group_queryset = queryset.values(group_field_name).annotate(count=Count("pk")).order_by(
+                *ordering).distinct()
+            page = self.paginate_queryset(group_queryset)
+            res_dict = {}
+            if require_total_count is None and require_group_count is None:
+                res_dict["totalCount"] = group_queryset.count()
             else:
-                group_field_name = group[0]["selector"].replace(".", "__")
-                ordering = self.get_ordering(group)
-                group_queryset = queryset.values(group_field_name).annotate(count=Count("pk")).order_by(
-                    *ordering).distinct()
-                page = self.paginate_queryset(group_queryset)
-                res_dict = {}
-                if require_total_count is None and require_group_count is None:
-                    res_dict["totalCount"] = group_queryset.count()
-                else:
-                    if require_group_count:
-                        res_dict["groupCount"] = group_queryset.count()
-                    if require_total_count:
-                        res_dict["totalCount"] = queryset.count()
-                if page is not None:
-                    res_dict["data"] = [{"key": x[group_field_name], "items": None, "count": x["count"]} for x in page]
-                    return Response(res_dict)
-                else:
-                    res_dict["data"] = [{"key": x[group_field_name], "items": None, "count": x["count"]} for x in
-                                        group_queryset]
+                if require_group_count:
+                    res_dict["groupCount"] = group_queryset.count()
+                if require_total_count:
+                    res_dict["totalCount"] = queryset.count()
+            if page is not None:
+                res_dict["data"] = [{"key": x[group_field_name], "items": None, "count": x["count"]} for x in page]
                 return Response(res_dict)
+            else:
+                res_dict["data"] = [{"key": x[group_field_name], "items": None, "count": x["count"]} for x in
+                                    group_queryset]
+            return Response(res_dict)
+
+    def _not_grouped_list(self, queryset):
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response({"data": serializer.data})
--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ setup(
     name='drf_dx_datagrid',
     version='0.1',
     packages=find_packages(),
-    url='',
-    license='',
-    author='sergey matveev',
+    url='https://github.com/SergeyMatveev88/drf-dx-datagrid',
+    license='MIT',
+    author='Sergey Matveev',
     author_email='dazranagon@yandex.ru',
-    description='Serverside realisation of grouping, filtering and sorting for DevExtreme datagrid on django rest framework'
+    description='Serverside realisation of grouping, filtering and sorting for DevExtreme datagrid on django rest framework',
+    install_requires=['djangorestframework']
 )
