8b081926c32512c1ca5c5449550404fabda842f0
Sergey <s.matveev@niitp.in>
2020-07-27 18:21:52 +0300

    fixed error with not list summary params

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='drf_dx_datagrid',
-    version='0.2.3',
+    version='0.2.4',
     packages=find_packages(),
     url='https://github.com/SergeyMatveev88/drf-dx-datagrid',
     license='MIT',
