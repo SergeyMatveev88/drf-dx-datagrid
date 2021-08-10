75dd6d70d1c836545911347cdaf269df6574258b
sergey <dazranagon@yandex.ru>
2021-08-10 22:31:47 +0300

    Added FILTER_CASE_SENISTIVE in README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='drf_dx_datagrid',
-    version='0.4.1',
+    version='0.4.2',
     packages=find_packages(),
     url='https://github.com/SergeyMatveev88/drf-dx-datagrid',
     license='MIT',
